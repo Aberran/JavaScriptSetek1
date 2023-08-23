1e61f8639bd147ba2da08670a604faa26a1972e9
VS <anaberrantis@gmail.com>
2023-08-23 10:42:17 +0200

    more apps

---
+++ functions.js
@@ -0,0 +1,65 @@
+// Funkcia na nacitanie dat z localStorage
+// osetrenie ak localStorage je prazdny
+
+const getSavedNames = () => {
+    const myNames = localStorage.getItem('names')
+
+    if(myNames !== null){
+        return JSON.parse(myNames)
+    } else {
+        return []
+    }
+
+}
+
+// Funckia pre puzitie pri odoslani formulara
+// Uklada do LocalSto0rage meno z fromulara
+
+const saveNames = (element) => {
+    localStorage.setItem('names', JSON.stringify(element))
+}
+
+// Generovanie HTML struktury, ktoru umiestnime do stranky po kliknuti na tlacitko 'vypis' + pouzijeme ju tiez pre vypisanie novych informacii z localStorage, ked neake meno vymazeme pomocou tlacitka Vymazat meno
+
+const genereateHTMLstructure = (oneName) => {
+   const newDiv = document.createElement('div')
+   const newSpan = document.createElement('span')
+   const button = document.createElement('button')
+
+    //nastavenie tlacitka
+
+    button.textContent = 'Vymaz meno'
+    newDiv.appendChild(button)
+
+    button.addEventListener('click', () => {
+        removeNames(names, oneName.id)
+        saveNames(names)
+        toListAgain()
+    })
+       
+
+    newSpan.textContent = oneName.firstName
+    newDiv.appendChild(newSpan)
+
+    return newDiv
+
+}
+
+//Podla ID najst id daneho mena -----splice ho odstrani
+
+const removeNames = (ourNames, id) => {
+    const index = ourNames.findIndex((nameWantToCheck) => {
+        return nameWantToCheck.id === id 
+    })
+
+    if(index > -1){
+        ourNames.splice(index, 1)
+    }
+
+}
+
+//ak zamzeme meno z local storage.....opat sa to vypise na stranke
+
+const toListAgain = () => {
+    let nextList = document.querySelector('.list-names').innerHTML = ''
+}
--- script.js
+++ script.js
@@ -1,81 +1,34 @@
+//Nacitanie dat z localStorage do premenne names
+//Ked je localStorage prazdny, tak do names sa ulozi prazdne pole
+
+const names = getSavedNames()
+
 let myForm = document.querySelector('#test-form')
 
-if(localStorage.getItem('criminals') == null){
-    var myArray = []
-} else{
-    myArray = JSON.parse(localStorage.getItem('criminals'))
-}
-
-// ukladanie do localstorage
 myForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
-    myArray.push({
-        id: '',
-        firstName: event.target.elements.first.value,
-        secondName: event.target.elements.second.value,
-        crime: event.target.elements.crime.value
+    names.push({
+        id: uuidv4(),
+        firstName: event.target.elements.firstName.value
     })
+    
+    event.target.elements.firstName.value = ''
 
-    //premazanie policka
-    event.target.elements.first.value = ''
-    event.target.elements.second.value = ''
-    event.target.elements.crime.value = ''
-
-    myArrayJSON = JSON.stringify(myArray)
-    localStorage.setItem('criminals', myArrayJSON)
-
+    saveNames(names)
 })
-
 
 // Vypisovanie do stranky
-let toList = document.querySelector('.to-list')
-toList.addEventListener('click', () => {
 
-    if(localStorage.getItem('criminals') === null){
-
-       let newPara = document.createElement('p')
-       newPara.textContent = 'Local storage je prazdny'
-       document.querySelector('.list-criminals').appendChild(newPara) 
-
-    } else {
-
-        let myStorage = localStorage.getItem('criminals')
-        let myStorageJ = JSON.parse(myStorage)
-
-        document.querySelector('.list-criminals').innerHTML = ''
-
-        myStorageJ.forEach(element => {
-        let newPara = document.createElement('p')
-        newPara.innerHTML = `Meno ${element.firstName}, <br> priezvisko ${element.secondName}, <br> zlocin ${element.crime}`
-        document.querySelector('.list-criminals').appendChild(newPara)
-        newPara.classList.add('basic-styles')
-    });
-    }
-})
-
-//Filtorvanie
-
-let nameFilter = document.querySelector('.name-filter')
-let myStorage = localStorage.getItem('criminals')
-let myStorageJ = JSON.parse(myStorage)
-
-nameFilter.addEventListener('input', (event) => {
-    let whatWeSearch = event.target.value
+let vypis = document.querySelector('.to-list')
+vypis.addEventListener('click', (event) => {
     
-    let myResults = myStorageJ.filter((event) => {
-        return event.firstName.toLowerCase().includes(whatWeSearch.toLowerCase())
-    })
+    let namesFromLS = localStorage.getItem('names')
+    let namesFromStoragesJSON = JSON.parse(namesFromLS)
 
-    document.querySelector('.filter-name').innerHTML = ''
-    document.querySelector('.list-criminals').innerHTML = ''
+    namesFromStoragesJSON.forEach(oneName => {
+        const oneNameHTML = genereateHTMLstructure(oneName)         
+        document.querySelector('.list-names').appendChild(oneNameHTML)
+    });
 
-    myResults.forEach((event) => {
-        let newPara = document.createElement('p')
-        newPara.innerHTML = `Prver meno: ${event.firstName}, <br>
-                            Priezvisko: ${event.secondName}, <br>
-                            Zlocin: ${event.crime}`
-        document.querySelector('.filter-name').appendChild(newPara)
-
-    })
 })
